e4cc71a556b46d09e03cec3b9d4ac6ed59cfc634
sinan <sinan@bpract.com>
2025-09-09 12:49:11 +0530

    feat: update

--- src/hooks/useAddSkelton.ts
+++ src/hooks/useAddSkelton.ts
@@ -1,11 +1,25 @@
 import React from "react";
+import createNodeWrapper from "../utils/create-node-wrapper";
+import { TEXT_TAGS } from "../constants/tags";
+import createLeafNode from "../utils/create-leaf-node";
 
 function useAddSkelton() {
   const addSkeleton = (node: React.ReactNode): React.ReactNode => {
-    if (!React.isValidElement(node)) return node;
+    if (!React.isValidElement(node)) return createNodeWrapper(node);
 
     const hasChildren = React.Children.count(node.props.children) > 0;
     const isValidChildren = typeof node.props.children !== "string";
+    const nodeType = typeof node.type;
+
+    if (nodeType === "function") {
+      return addSkeleton(node.type(node.props));
+    }
+    if (TEXT_TAGS.includes(node.type)) {
+      return createLeafNode({ node, className: "skeleton skeleton-text" });
+    }
+    if (node.type === "img") {
+      return createNodeWrapper(node);
+    }
 
     if (hasChildren && isValidChildren) {
       const childWithSkeletons = React.Children.map(
@@ -18,10 +32,7 @@ function useAddSkelton() {
         children: childWithSkeletons,
       });
     } else {
-      return React.cloneElement(node, {
-        ...node.props,
-        className: (node.props.className || "") + " skeleton",
-      });
+      createLeafNode({ node });
     }
   };
 
